64588d9e93678db347626b02b75c0a5a7a9e7fbc
notfarg <eliza.m.grafton@gmail.com>
2019-10-28 17:29:38 -0400

    Removed InControl in Favour of GamePadInput, created basic playermotor

---
+++ Project/Sprint Week 2019/Assets/Scripts/Players/PlayerMotor.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GamepadInput;
+public class PlayerMotor : MonoBehaviour
+{
+    public GamePad.Index playerIndex;
+    public Rigidbody2D rb;
+    public Vector2 moveDir;
+    public Vector2 lastMoveDir;
+    public Vector2 aimDir;
+    public Vector2 lastAimDir;
+    public float maxSpeed;
+    public float currentSpeed;
+    float accel;
+    public float accelTime;
+    public float decelTime;
+    public bool isStarting;
+    public bool isStopping;
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        if (moveDir == Vector2.zero && currentSpeed > 0 && !isStopping)
+        {
+            CalcAccel(0, decelTime);
+            isStopping = true;
+            isStarting = false;
+        }
+        if (moveDir != Vector2.zero && currentSpeed < maxSpeed && !isStarting)
+        {
+            CalcAccel(maxSpeed, decelTime);
+            isStopping = false;
+            isStarting = true;
+        }
+        
+        if (isStarting || isStopping)
+        {
+            Accelerate();
+        }
+
+        if (moveDir != Vector2.zero)
+        {
+            lastMoveDir = moveDir;
+        }
+
+        if (aimDir != Vector2.zero)
+        {
+            lastAimDir = aimDir;
+        }
+
+        rb.velocity = lastMoveDir.normalized * currentSpeed;
+        transform.up = lastAimDir.normalized;
+    }
+
+    public void CheckInput()
+    {
+        moveDir = GamePad.GetAxis(GamePad.Axis.LeftStick, playerIndex);
+        aimDir = GamePad.GetAxis(GamePad.Axis.RightStick, playerIndex);
+    }
+
+    public void CalcAccel(float trgtSpd, float timeToAccel)
+    {
+        accel = (trgtSpd - currentSpeed) / timeToAccel;
+    }
+
+    public void Accelerate()
+    {
+        currentSpeed += accel * Time.deltaTime;
+        currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeed);
+        if (currentSpeed == 0 || currentSpeed == maxSpeed)
+        {
+            isStopping = false;
+            isStarting = false;
+        }
+    }
+}
