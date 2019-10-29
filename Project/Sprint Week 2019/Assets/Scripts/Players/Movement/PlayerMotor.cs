f1b316dcba0af6f9fc8f441c13f01ba94251a219
notfarg <eliza.m.grafton@gmail.com>
2019-10-29 13:57:16 -0400

    fixed player rotation

--- Project/Sprint Week 2019/Assets/Scripts/Players/Movement/PlayerMotor.cs
+++ Project/Sprint Week 2019/Assets/Scripts/Players/Movement/PlayerMotor.cs
@@ -50,7 +50,7 @@ public class PlayerMotor : MonoBehaviour
         }
 
         rb.velocity = lastMoveDir.normalized * currentSpeed;
-        transform.up = lastAimDir.normalized;
+        transform.rotation = Quaternion.Euler(0,0,Mathf.Atan2(lastAimDir.normalized.y, lastAimDir.normalized.x));
     }
 
     public void CheckInput()
