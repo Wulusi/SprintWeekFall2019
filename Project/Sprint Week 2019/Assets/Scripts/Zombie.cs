5373d18c9da05bf9315f56d5303dc2a7d3a26869
notfarg <eliza.m.grafton@gmail.com>
2019-10-30 12:35:05 -0400

    Pathing on grid for zombies

--- Project/Sprint Week 2019/Assets/Scripts/Zombie.cs
+++ Project/Sprint Week 2019/Assets/Scripts/Zombie.cs
@@ -5,38 +5,46 @@ using UnityEngine;
 public class Zombie : MonoBehaviour
 {
     Vector2 dirToWalk;
-    public GameObject targetPoint;
+    public int targetPoint = 0;
+
+    public Vector2[] pathToFollow;
     public float walkSpd;
     public Rigidbody2D rb;
     public float damageVal, forcePushAmt;
 
     private void Update()
     {
-        if (targetPoint != null)
+
+        dirToWalk = pathToFollow[targetPoint] - (Vector2)transform.position;
+        rb.velocity = Vector2.Lerp(rb.velocity, dirToWalk.normalized * walkSpd, 0.1f);
+
+        if (Vector2.Distance(pathToFollow[targetPoint], transform.position) < 0.1f)
         {
-            dirToWalk = targetPoint.transform.position - transform.position;
-            rb.velocity = Vector2.Lerp(rb.velocity, dirToWalk.normalized * walkSpd, 0.1f);
-        } else
-        {
-            GameObject[] allBasesLeft = GameObject.FindGameObjectsWithTag("Base");
-            if (allBasesLeft.Length == 0)
+            if (targetPoint < pathToFollow.Length - 1)
             {
-                //players have lost
-            } else
-            {
-                int indexWithShortestDistance = 0;
-                float shortestDistance = Mathf.Infinity;
-                for (int i = 0; i < allBasesLeft.Length; i++)
-                {
-                    if (Vector2.Distance(transform.position, allBasesLeft[i].transform.position) < shortestDistance)
-                    {
-                        indexWithShortestDistance = i;
-                        shortestDistance = Vector2.Distance(transform.position, allBasesLeft[i].transform.position);
-                    }
-                }
-                targetPoint = allBasesLeft[indexWithShortestDistance];
+                targetPoint++;
             }
         }
+
+        //GameObject[] allBasesLeft = GameObject.FindGameObjectsWithTag("Base");
+        //if (allBasesLeft.Length == 0)
+        //{
+        //    //players have lost
+        //} else
+        //{
+        //    int indexWithShortestDistance = 0;
+        //    float shortestDistance = Mathf.Infinity;
+        //    for (int i = 0; i < allBasesLeft.Length; i++)
+        //    {
+        //        if (Vector2.Distance(transform.position, allBasesLeft[i].transform.position) < shortestDistance)
+        //        {
+        //            indexWithShortestDistance = i;
+        //            shortestDistance = Vector2.Distance(transform.position, allBasesLeft[i].transform.position);
+        //        }
+        //    }
+        //    targetPoint = allBasesLeft[indexWithShortestDistance].transform.position;
+        //}
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -45,9 +53,9 @@ public class Zombie : MonoBehaviour
         {
 
         }
-        else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Bases")|| collision.collider.gameObject.layer == LayerMask.NameToLayer("Obstacles"))
+        else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Bases") || collision.collider.gameObject.layer == LayerMask.NameToLayer("Obstacles"))
         {
-            collision.collider.GetComponent<Health>().TakeDamage(damageVal,gameObject.GetComponent<Health>().elementType);
+            collision.collider.GetComponent<Health>().TakeDamage(damageVal, gameObject.GetComponent<Health>().elementType);
         }
         rb.AddForce((transform.position - collision.collider.transform.position) * forcePushAmt, ForceMode2D.Impulse);
     }
